82df68f27e3a42a9b1487e263ed04cc42f30977f
LukeAir <LukeAirven007@protonmail.com>
2025-02-07 17:10:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'caba07d4-f4c2-d77f-80a9-87df80f1b643';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
